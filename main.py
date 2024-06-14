23bc87b6d4e9c457eb2f0a40570df5aba0bd481c
pavelmeysak <pavelmeysak@gmail.com>
2024-06-14 16:27:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+a = "Первое задание для обучения"
+print("Ответ на первую задачу:", len(a))
+first = 3
+second = 5
+summa = first + second
+diff = first - second
+print("Ответ на вторую задачу:", "Сумма: " + str(summa) + ", " "Разность: " + str(diff))
+first = 4
+second = 8
+third = 6
+mean = (first + second + third)/3
+print("Ответ на третью задачу:", mean)
+first_string = "Вторник"
+second_string = "Понедельник"
+print("Ответ на четвертую задачу:", second_string + ", " + first_string)
+a = 10
+b = 17
+c = 14
+f = (a*b) + (a*c)
+print("Ответ на пятую задачу:", (f**3)/2)
